4e780818556c733323c768a534c87e41a15bbd6c
Helycheng <hoolooday@gmail.com>
2015-10-04 22:02:02 +0800

    +ListView间隔行变色
    WinRt 中Setter不能再Binding啦

---
+++ Converters/ListItemBackgroudConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace SLWeek.Converters
+{
+   public  sealed class ListItemBackgroudConverter:IValueConverter
+    {
+       public object Convert(object value, Type targetType, object parameter, string language)
+       {
+           var item = value as ListViewItem;
+           if (item != null)
+           {
+                var listView = ItemsControl.ItemsControlFromItemContainer(item);
+                var index = listView.ItemContainerGenerator.IndexFromContainer(item);
+                return index % 2 == 0 ? new SolidColorBrush() { Color = Colors.DarkRed } : new SolidColorBrush() { Color = Colors.LightCyan };
+            }
+
+           return null;
+       }
+
+       public object ConvertBack(object value, Type targetType, object parameter, string language)
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
---
+++ Converters/ListViewItemStyleSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace SLWeek.Converters
+{
+    public   class ListViewItemStyleSelector: StyleSelector
+    {
+        protected override Style SelectStyleCore(System.Object item, DependencyObject container)
+        {
+            var style = new Style {TargetType = typeof (ListViewItem)};
+            Setter backgroudSetter = new Setter {Property = Windows.UI.Xaml.Controls.Control.BackgroundProperty};
+            var listview = ItemsControl.ItemsControlFromItemContainer(container) as ListView;
+            if (listview != null)
+            {
+                var index = listview.IndexFromContainer(container);
+                backgroudSetter.Value = index%2==0 ? new SolidColorBrush() {Color = Colors.Aqua} : new SolidColorBrush() { Color = Colors.Red };
+            }
+            style.Setters.Add(backgroudSetter);
+            return style;
+        }
+    }
+}
